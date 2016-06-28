f3e6bae20b60c2930921944bfdc5e3e728fc45a9
Pengcheng Nie <pengcheng1995@gmail.com>
2016-06-28 14:56:40 +0800

    new

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,9 +1,11 @@
 var menu = [
-	['就两个网页',
+	['就一些网页',
 	['demo/learn/6-11/cai.html','猜拳'],
 	['demo/learn/6-14/baidu.html', 'copy百度'],
-    ['demo/ife/task-1/1-2/1-2.html', '网页布局'],
-    ['demo/ife/task-1/1-3/1-3.html', '三栏分局']
+    ['demo/ife/task-1/1-2/1-2.html', '1.网页布局'],
+    ['demo/ife/task-1/1-3/1-3.html', '2.三栏分局'],
+    ['demo/ife/task-1/1-4/1-4.html', '3.定位居中'],
+    ['demo/ife/task-1/1-5/1-5.html', '5.网页布局2']
 	]
 ];
 var list = "";
