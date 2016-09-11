c649c9a260780f9caa7c61365c4a2db1bed6b506
Pengcheng Nie <pengcheng1995@gmail.com>
2016-09-11 17:55:13 +0800

    pic

--- demo/learn/9-10/pic.js
+++ demo/learn/9-10/pic.js
@@ -10,10 +10,12 @@
 
     var imgData = ctx.getImageData(0, 0, c.width, c.height);
     for (var i = 0; i < imgData.data.length; i+=4) {
-    	imgData.data[i] = 255 - imgData[i];
-    	imgData.data[i+1] = 255 - imgData[i+1];
-    	imgData.data[i+2] = 255 - imgData[i+2];
-    	imgData.data[i+3] = 255;
+    	var r = imgData[i];
+      var g = imgData[i+1];
+      var b = imgData[i+2];
+      var gray = (r*30 + g*59 +b*11 +50)/100;
+      imgData[i] = imgData[i+1] = imgData[i+2] = gray;
+      imgData[i+3] = 255;
     }
 
     ctx.putImageData(imgData, 0, 0);
